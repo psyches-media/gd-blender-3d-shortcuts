609782c95bd9ca82333f28281cdbc232252acbf3
Alfonso J. Ramos <theraot@gmail.com>
2025-06-10 12:53:17 -0500

    Whitespace

--- addons/gd-blender-3d-shortcuts/Utils.gd
+++ addons/gd-blender-3d-shortcuts/Utils.gd
@@ -35,7 +35,7 @@ static func reset_scale(nodes):
 		var quat = node.transform.basis.get_rotation_quaternion()
 		node.transform.basis = Basis(quat)
 
-static func hide_nodes(nodes, is_hide=true):
+static func hide_nodes(nodes, is_hide = true):
 	for node in nodes:
 		node.visible = !is_hide
 
@@ -105,7 +105,7 @@ static func get_spatial_editor_local_space_button(spatial_editor):
 	for child in children:
 		if child.get_class() == "Button":
 			if child.shortcut:
-				if child.shortcut.get_as_text() == OS.get_keycode_string(KEY_T):# TODO: Check if user has custom shortcut
+				if child.shortcut.get_as_text() == OS.get_keycode_string(KEY_T): # TODO: Check if user has custom shortcut
 					return child
 
 static func get_spatial_editor_snap_button(spatial_editor):
@@ -113,7 +113,7 @@ static func get_spatial_editor_snap_button(spatial_editor):
 	for child in children:
 		if child.get_class() == "Button":
 			if child.shortcut:
-				if child.shortcut.get_as_text() == OS.get_keycode_string(KEY_Y):# TODO: Check if user has custom shortcut
+				if child.shortcut.get_as_text() == OS.get_keycode_string(KEY_Y): # TODO: Check if user has custom shortcut
 					return child
 
 static func project_on_plane(camera, screen_point, plane):
@@ -167,15 +167,15 @@ static func draw_dashed_line(canvas_item, from, to, color, width, dash_length = 
 	var length = (to - from).length()
 	var normal = (to - from).normalized()
 	var dash_step = normal * dash_length
-	
-	if length < dash_length: #not long enough to dash
+
+	if length < dash_length: # not long enough to dash
 		canvas_item.draw_line(from, to, color, width, antialiased)
 		return
 
 	else:
 		var draw_flag = true
 		var segment_start = from
-		var steps = length/dash_length
+		var steps = length / dash_length
 		for start_length in range(0, steps + 1):
 			var segment_end = segment_start + dash_step
 			if draw_flag:
@@ -183,6 +183,6 @@ static func draw_dashed_line(canvas_item, from, to, color, width, dash_length = 
 
 			segment_start = segment_end
 			draw_flag = !draw_flag
-		
+
 		if cap_end:
 			canvas_item.draw_line(segment_start, to, color, width, antialiased)
